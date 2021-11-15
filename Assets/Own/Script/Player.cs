b17b8f60adf2d1e678b12f70a90e7e1d16594bc7
Xelez <kiasund@live.se>
2021-11-15 16:18:45 +0100

    Added player

---
+++ Assets/Own/Script/Player.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * .1f, moveVertical *.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
